b401ee39068abbcc9383034c67378b4e4a541f42
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-16 11:00:00 +0300

    Login init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <h2>You Need to Login </h2>
+      )}
     </div>
   );
 }
