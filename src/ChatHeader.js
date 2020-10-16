33a84f1a84ed9e711144bca7cd5d35182df42129
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-16 10:19:02 +0300

    Chat Header final

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,7 +1,33 @@
 import React from "react";
+import NotificationIcon from "@material-ui/icons/Notifications";
+import EditLocationRoundedIcon from "@material-ui/icons/EditLocationRounded";
+import PeopleAltRoundedIcon from "@material-ui/icons/PeopleAltRounded";
+import SearchroundedIcon from "@material-ui/icons/SearchRounded";
+import SendRoundedIcon from "@material-ui/icons/SendRounded";
+import HelpRoundedICon from "@material-ui/icons/HelpRounded";
 import "./ChatHeader.css";
 function ChatHeader() {
-  return <div className="chatHeader"></div>;
+  return (
+    <div className="chatHeader">
+      <div className="chatHeader__left">
+        <h3>
+          <span className="chatHeader__hash">#</span>Room Name
+        </h3>
+      </div>
+      <div className="chatHeader__right">
+        <NotificationIcon />
+        <EditLocationRoundedIcon />
+        <PeopleAltRoundedIcon />
+
+        <div className="chatHeader__search">
+          <input type="text" placeholder="search" />
+          <SearchroundedIcon />
+        </div>
+        <SendRoundedIcon />
+        <HelpRoundedICon />
+      </div>
+    </div>
+  );
 }
 
 export default ChatHeader;
