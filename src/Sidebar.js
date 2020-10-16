0fdb7149e2155d96e0779855a597876e4a6fc860
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-16 09:39:48 +0300

    SidebarChannel init

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,31 @@
 import React from "react";
+import AddIcon from "@material-ui/icons/Add";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import "./Sidebar.css";
+import SidebarChannel from "./SidebarChannel";
 
 function Sidebar() {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Room Name</h3>
+        <ExpandMoreIcon />
+      </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel" />
+        </div>
+      </div>
+      <div className="sidebar__channelsList">
+        <SidebarChannel />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
