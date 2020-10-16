fc972c7554c65ee8b97a5ba9d8d1e6db4cc9c5f2
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-16 10:43:54 +0300

    Firebase init

---
+++ src/Firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNRgPV_TOaCsejLySduIGnlsiJZbVzzEM",
+  authDomain: "discord-clone-5aebf.firebaseapp.com",
+  databaseURL: "https://discord-clone-5aebf.firebaseio.com",
+  projectId: "discord-clone-5aebf",
+  storageBucket: "discord-clone-5aebf.appspot.com",
+  messagingSenderId: "489519371965",
+  appId: "1:489519371965:web:c13fb3d719ccbf641b49d4",
+  measurementId: "G-C2P7W35XBD",
+};
+
+const firebaseApp = firebase.initialiseApp(firebaseConfig);
+const db = firebaseApp.firestorew();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
--- src/Message.js
+++ src/Message.js
@@ -1,8 +1,20 @@
+import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Message.css";
 
 function Message() {
-  return <div className="message"></div>;
+  return (
+    <div className="message">
+      <Avatar />
+      <div className="message__info">
+        <h4>
+          Patrick Wehbe
+          <span className="message__timestamp">This is a timestamp</span>
+        </h4>
+        <p>This is the message</p>
+      </div>
+    </div>
+  );
 }
 
 export default Message;
